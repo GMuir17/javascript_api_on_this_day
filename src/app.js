efc18bd12bfc506db77f1b42de7bf362750795fd
Gary Muir <garymuir17@gmail.com>
2018-08-10 17:45:28 +0100

    adds api access

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const Events = require("./models/events.js");
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hey");
 
   const events = new Events();
   events.getData();
--- src/models/events.js
+++ src/models/events.js
@@ -14,6 +14,19 @@ function timeStamp() {
 Events.prototype.getData = function () {
   const date = timeStamp();
   console.log("date", date);
+
+  const url = "https://history.muffinlabs.com/date";
+  const request = new Request(url);
+
+  request.get()
+    .then((data) => {
+      this.data = data.message;
+      console.log(data.data["Events"]);
+      PubSub.publish("Events:event-data-ready", this.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 
 
