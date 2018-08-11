d61de59a23d5fd8b92f8c056da850a1b22b08847
Gary Muir <garymuir17@gmail.com>
2018-08-11 16:44:01 +0100

    adds bind events for select_view

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const Events = require("./models/events.js");
 const EventListView = require("./views/event_list_view.js");
+const SelectView = require("./views/select_view.js");
 
 document.addEventListener("DOMContentLoaded", () => {
 
   const eventListContainer = document.querySelector("#event-container");
   const eventListView = new EventListView(eventListContainer);
   eventListView.bindEvents();
+
+  const selectView = new SelectView();
+  selectView.bindEvents();
 
   const events = new Events();
   events.getTodaysData();
--- src/views/select_view.js
+++ src/views/select_view.js
@@ -1,10 +1,14 @@
 const PubSub = require("../helpers/pub_sub.js");
 
-const SelectView = function () {
+const SelectView = function () {};
 
+SelectView.prototype.bindEvents = function () {
+  const inputField = document.querySelector("#date");
+  inputField.addEventListener("change", (evt) => {
+    const selectedDate = evt.target.value;
+    console.log(selectedDate);
+  })
 };
-
-
 
 
 
